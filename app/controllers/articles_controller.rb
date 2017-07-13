cbbcaa907ee9b205f313011bc475acbf493a05bf
Jason Clay <anticlergygang@gmail.com>
2017-07-13 18:58:23 -0400

    process fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
 			session = GoogleDrive::Session.from_config("config.json")
 			ws = session.spreadsheet_by_key("1klCFgSwetuuQlkM4llE2hjXQcVZX-wUNkDaWWoVs7ds").worksheets[0]
 			ws[(ws.rows.length + 1), 1] = @article.articleUrl
-			ws[(ws.rows.length + 1), 2] = 'process'
+			ws[(ws.rows.length), 2] = 'process'
 			ws.save
 			render json: @article
 		else
