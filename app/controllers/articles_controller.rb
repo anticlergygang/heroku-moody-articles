7c1a4fa8abffcf93872017396fb6f3b947a9dcd2
Jason Clay <anticlergygang@gmail.com>
2017-07-13 19:31:54 -0400

    whoothuritis

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
 		if @article.save
 			session = GoogleDrive::Session.from_config("config.json")
 			ws = session.spreadsheet_by_key("1klCFgSwetuuQlkM4llE2hjXQcVZX-wUNkDaWWoVs7ds").worksheets[0]
+			ws.reload
 			ws[(ws.rows.length + 1), 1] = @article.articleUrl
 			ws[(ws.rows.length), 2] = 'process'
 			ws.save
