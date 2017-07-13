72bfa6504001c2bdab1819adf1cee01a8ce958ff
Jason Clay <anticlergygang@gmail.com>
2017-07-13 18:39:27 -0400

    google test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
 		if @article.save
 			session = GoogleDrive::Session.from_config("config.json")
 			ws = session.spreadsheet_by_key("1klCFgSwetuuQlkM4llE2hjXQcVZX-wUNkDaWWoVs7ds").worksheets[0]
-			ws[(ws.rows.length + 1), 1] = article_url.articleUrl
+			ws[(ws.rows.length + 1), 1] = @article.articleUrl
 			render json: @article
 		else
 			render json: @article.errors, status: :unprocessable_entity
