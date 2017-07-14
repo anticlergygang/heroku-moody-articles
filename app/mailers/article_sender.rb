438e9a76a05ea064e5d416115192d38573f87872
Jason Clay <anticlergygang@gmail.com>
2017-07-13 23:39:32 -0400

    articleUrl

--- app/mailers/article_sender.rb
+++ app/mailers/article_sender.rb
@@ -3,6 +3,7 @@ class ArticleSender < ApplicationMailer
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_article(article)
+  	@article = article
     mail( :to => 'jasonac1994@gmail.com',
     :subject => 'Article url' )
   end
