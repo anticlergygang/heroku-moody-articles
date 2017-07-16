157ff0452de5d2976d99ccf6e4715c9ca39eca37
Jason Clay <anticlergygang@gmail.com>
2017-07-16 19:43:56 -0400

    how do i access youuuu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 	def index
 		@articles = Article.all()
 		if params[:orderFilta]
-			puts params[:order]
+			puts :order
 		else
 			puts 'no order'
 		end
