69631795431fb9ff8d107f11453d5ecc6952d049
Jason Clay <anticlergygang@gmail.com>
2017-07-17 00:07:42 -0400

    this is it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,7 @@ class ArticlesController < ApplicationController
 		numRows = ws.num_rows + 1
 		while itt < numRows  do
 		   puts ws[itt,1]
-		   art = Article.where(articleUrl: ws[itt,1])
-		   puts art
+		   puts Article.where(articleUrl: ws[itt,1])[:articleUrl]
 		   itt +=1
 		end
 	end
